a26acf3651b6816ba1f62d6eebbb1cb128a552ab
TAIsRich <Richard.lyu0120@gmail.com>
2022-06-12 17:41:55 -0700

    ArrayLIst

---
+++ java-core/src/main/java/com/chuwa/exercise/collection/ArrayListExerciseTest.java
@@ -0,0 +1,36 @@
+package com.chuwa.exercise.collection;
+
+import org.junit.Test;
+
+/**
+ * @author b1go
+ * @date 6/12/22 4:43 PM
+ */
+public class ArrayListExerciseTest {
+
+    /**
+     * new ArrayList()
+     * add elements
+     * get element
+     * get Size
+     */
+    @Test
+    public void learnInsertingAndRetrieving() {
+
+    }
+
+    /**
+     * remove(int index)
+     * remove(Object o)
+     * removeRange(int fromIndex, int toIndex)
+     * removeAll(Collection<?> c)
+     * clear()
+     * replaceAll(UnaryOperator<E> operator)
+     */
+    @Test
+    public void learnRemoveReplacingUpdating() {
+
+    }
+
+
+}
