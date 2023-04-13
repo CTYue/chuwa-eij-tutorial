030f808835f8f1465dc334a96fb75525a89a44b0
TAIsRich <Richard.lyu0120@gmail.com>
2023-04-12 17:56:55 -0700

    add comments

--- 02-java-core/src/main/java/com/chuwa/tutorial/t06_java8/features/default_interface_method/DIML.java
+++ 02-java-core/src/main/java/com/chuwa/tutorial/t06_java8/features/default_interface_method/DIML.java
@@ -3,12 +3,15 @@ package com.chuwa.tutorial.t06_java8.features.default_interface_method;
 public interface DIML {
     static final String BLOG = "is Chuwa a";
 
+    // abstract method
     int add(int a, int b);
 
+    // default method
     default int substract(int a, int b) {
         return a - b;
     }
 
+    // static method
     static String blogName() {
         return BLOG;
     }
