31c6645595228657afc632a0585a6b8472ba9a34
bigo <yanan.lyu@gmail.com>
2023-06-25 16:16:28 -0700

    git init for ood

---
+++ m04_ood/src/main/java/com/chuwa/m04_ood/M04OodApplication.java
@@ -0,0 +1,13 @@
+package com.chuwa.m04_ood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M04OodApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(M04OodApplication.class, args);
+    }
+
+}
---
+++ m04_ood/src/test/java/com/chuwa/m04_ood/M04OodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chuwa.m04_ood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class M04OodApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
