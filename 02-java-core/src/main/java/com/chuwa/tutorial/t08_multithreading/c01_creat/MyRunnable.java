56867a34abc5b829c73a271667f94129b71630c6
CAnonymous <zidong.yu@outlook.com>
2024-06-21 16:41:38 -0700

    add Thread.sleep to Runnable Example demonstrate that exceptions should be handled within Runnable.

--- 02-java-core/src/main/java/com/chuwa/tutorial/t08_multithreading/c01_creat/MyRunnable.java
+++ 02-java-core/src/main/java/com/chuwa/tutorial/t08_multithreading/c01_creat/MyRunnable.java
@@ -8,5 +8,10 @@ public class MyRunnable implements Runnable{
     @Override
     public void run() {
         System.out.println("Start new thread using Runnable");
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
