aa82b17d757b6664cd2c1992852bfb189c77d036
CAnonymous <zidong.yu@outlook.com>
2025-06-11 11:44:39 -0700

    add more demo code.

--- 02-java-core/src/main/java/com/chuwa/tutorial/t01_basic/MethodArea.java
+++ 02-java-core/src/main/java/com/chuwa/tutorial/t01_basic/MethodArea.java
@@ -1,6 +1,8 @@
 package com.chuwa.tutorial.t01_basic;
 
 import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MethodArea {
     public static void main(String[] args) {
@@ -11,9 +13,27 @@ public class MethodArea {
         Method[] a0m = a0Class.getDeclaredMethods();
         Method[] a1m = a1Class.getDeclaredMethods();
 
+        //Demo method area: class information (meta data), methods...
+        //method area is called "metaspace" since JDK 8
         for(int i=0;i<a0m.length;i++){
             System.out.println("a0m[i].hashCode(): "+ a0m[i].hashCode()+" a1m[i].hashCode(): "+ a1m[i].hashCode());
             System.out.println(a0m[i].equals(a1m[i]));
+        }
+
+        //Demo method area and heap memory
+        List objectList = new ArrayList<>();
+        for(int i=0;i<5;i++) {
+            B b = new B();
+            objectList.add(b);
+            System.out.println("b.hashCode(): "+ b.hashCode()+ " b.getClass().hashCode(): " + b.getClass().hashCode());
+        }
+
+        //Demo String constant pool
+        String str = "chuwa";
+        System.out.println("str.hashCode(): "+ str.hashCode());
+        for(int i=0;i<5;i++){
+            str = "chuwa";
+            System.out.println("str.hashCode(): "+ str.hashCode());
         }
     }
 }
