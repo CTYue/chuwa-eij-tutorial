bafaafbf4763e8086c7da4d1ef0eba663f40e958
CAnonymous <zidong.yu@outlook.com>
2025-06-16 20:00:02 -0700

    Demo why default method is important.

--- 02-java-core/src/main/java/com/chuwa/tutorial/t02_oop/abstractclass_interface/American.java
+++ 02-java-core/src/main/java/com/chuwa/tutorial/t02_oop/abstractclass_interface/American.java
@@ -1,9 +1,15 @@
 package com.chuwa.tutorial.t02_oop.abstractclass_interface;
 
 interface American extends People{
-    String str = "sss";
+    static String str = "sss";
     public void say();
     default void speakEnglish() {
         System.out.println("Speaking English");
+    }
+
+    //Demo why default method is important
+    default void whereIam() {
+        Class c = this.getClass();
+        System.out.println("I am in " + c.getName());
     }
 }
