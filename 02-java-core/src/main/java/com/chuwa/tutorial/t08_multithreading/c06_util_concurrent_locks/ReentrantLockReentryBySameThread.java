078ab58414fa957162c16ed8976fb96d5ffe5e17
CAnonymous <zidong.yu@outlook.com>
2025-06-24 01:29:39 -0700

    add more reentrant lock demo.

--- 02-java-core/src/main/java/com/chuwa/tutorial/t08_multithreading/c06_util_concurrent_locks/ReentrantLockReentryBySameThread.java
+++ 02-java-core/src/main/java/com/chuwa/tutorial/t08_multithreading/c06_util_concurrent_locks/ReentrantLockReentryBySameThread.java
@@ -3,45 +3,51 @@ package com.chuwa.tutorial.t08_multithreading.c06_util_concurrent_locks;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.locks.ReentrantLock;
 
-public class ReentrantLockReentryBySameThread {
-    private final ReentrantLock lock = new ReentrantLock();
+class MyReentrantLock {
+    public static final ReentrantLock lock = new ReentrantLock();
+}
 
-    public void entry() {
-        lock.lock();
+public class ReentrantLockReentryBySameThread extends Thread {
+
+    public void run() {
+        MyReentrantLock.lock.lock();//Acquire lock
         try {
             System.out.println("Entered entry area, acquired lock...");
+            System.out.println("Current Thread ID:" + Thread.currentThread().getId());
             reentry();
         } finally {
-            lock.unlock();
+            MyReentrantLock.lock.unlock();
             System.out.println("Exited entry area, released lock...");
         }
     }
 
     public void reentry() {
-        lock.lock();
+        MyReentrantLock.lock.lock();//Acquire lock again
         try{
             System.out.println("Entered reentry area, acquired lock AGAIN...");
             rereentry();
         } finally {
-            lock.unlock();
+            MyReentrantLock.lock.unlock();
             System.out.println("Exited reentry area, released lock...");
         }
     }
 
     public void rereentry() {
-        lock.lock();
+        MyReentrantLock.lock.lock();//Acquire lock again again
         try{
             System.out.println("Entered rereentry area, acquired lock AGAIN...");
         } finally {
-            lock.unlock();
+            MyReentrantLock.lock.unlock();
             System.out.println("Exited rereentry area, released lock...");
         }
     }
 
     public static void main(String[] args) throws InterruptedException, ExecutionException {
         ReentrantLockReentryBySameThread reentry = new ReentrantLockReentryBySameThread();
-        Thread t = new Thread(reentry::entry);
-        t.start();
-        t.join();
+        Thread[] ts = new Thread[]{new ReentrantLockReentryBySameThread(), new ReentrantLockReentryBySameThread()};
+        for(Thread t: ts)
+            t.start();
+        for(Thread t: ts)
+            t.join();
     }
 }
